edea798c74003de61d9a100ef629fc62017e754b
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-08 11:00:24 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Title, Description, Illutration, Logo } from './styles';
+
+export const Main = ({ title = 'React Avançado' }) => {
+  return (
+    <Container>
+      <Logo src="/img/logo.svg" alt="Logo React Avançado" />
+      <Title>{title}</Title>
+      <Description>Typescript, ReactJS, NextJS e Styled Components</Description>
+      <Illutration
+        src="/img/hero-illustration.svg"
+        alt="Um dev com a tela do código"
+      />
+    </Container>
+  );
+};
