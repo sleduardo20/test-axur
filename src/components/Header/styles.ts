c367c44c0f163be8c4907907dd191fbb2f5324b3
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-08 12:05:49 -0300

    💄 styles (header component): add styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.main`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 10rem;
+    background-color: ${theme.colors.white};
+    padding: ${theme.spacings.md};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      margin-top: ${theme.spacings.xs};
+      color: ${theme.colors.secondary};
+      text-align: center;
+    }
+  `}
+`;
