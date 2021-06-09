40d59653704d303063c47c8297fb3f1941be5a4a
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-09 20:46:25 -0300

    ⚡ feat (ContentIndex): make validation errors input form

--- src/components/ContentIndex/index.tsx
+++ src/components/ContentIndex/index.tsx
@@ -11,39 +11,69 @@ import { useInspections } from '../../hooks/useInspections';
 import * as S from './styles';
 
 type FormData = {
-  keyword: string;
+  name: string;
 };
 
 export const ContentIndex = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const { createInspections, inspections, loading } = useInspections();
 
-  const handleCreateInspection: SubmitHandler<FormData> = async values => {
-    createInspections(values.keyword);
+  const handleCreateInspection: SubmitHandler<FormData> = async (
+    data,
+    event,
+  ) => {
+    createInspections(data.name);
+
+    event?.target.reset();
   };
 
   return (
     <Wrapper>
       <S.Container>
+        <h2>Cadastro de Inspeções</h2>
         <form onSubmit={handleSubmit(handleCreateInspection)}>
-          <Input placeholder="Nome da inspeção..." {...register('keyword')} />
+          <S.Field>
+            <Input
+              placeholder="Nome da inspeção..."
+              {...register('name', {
+                required: { value: true, message: 'Digite um algum nome.' },
+                minLength: { value: 4, message: 'No mínimo 4 caracteres.' },
+              })}
+            />
+
+            {errors.name && errors.name.type === 'required' && (
+              <span>{errors.name.message}</span>
+            )}
+            {errors.name && errors.name.type === 'minLength' && (
+              <span>{errors.name.message}</span>
+            )}
+          </S.Field>
+
           <Button color="secondary" type="submit">
             {loading ? <Loader /> : 'Cadastrar'}
           </Button>
         </form>
 
-        {inspections.length > 0 && <h3>Listagem de Inspeções:</h3>}
+        {inspections.length > 0 && (
+          <>
+            <h3>Listagem de Inspeções:</h3>
 
-        <ul>
-          {inspections.map(inspection => (
-            <Inspection
-              key={inspection.idInspection}
-              id={inspection.idInspection}
-              nameInspection={inspection.nameInspection}
-            />
-          ))}
-        </ul>
+            <ul>
+              {inspections.map(inspection => (
+                <Inspection
+                  key={inspection.idInspection}
+                  id={inspection.idInspection}
+                  nameInspection={inspection.nameInspection}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </S.Container>
     </Wrapper>
   );
--- src/components/ContentIndex/styles.ts
+++ src/components/ContentIndex/styles.ts
@@ -3,10 +3,15 @@ import media from 'styled-media-query';
 
 export const Container = styled.main`
   ${({ theme }) => css`
+    h2 {
+      color: ${theme.colors.darkGray};
+      font-size: ${theme.font.size.md};
+      margin-bottom: ${theme.spacings.xs};
+    }
     h3 {
       color: ${theme.colors.secondary};
       font-size: ${theme.font.size.md};
-      margin: ${theme.spacings.xxlg} 0;
+      margin: ${theme.spacings.xxlg} 0 ${theme.spacings.lg} 0;
     }
 
     ul {
@@ -21,14 +26,27 @@ export const Container = styled.main`
 
     form {
       width: 100%;
+      height: 12rem;
+      padding: ${theme.spacings.sm};
       display: flex;
       margin: 0 auto;
       align-items: center;
       justify-content: space-between;
+      border: 1px solid ${theme.colors.darkGray};
+      border-radius: 0.4rem;
 
       ${media.lessThan('medium')`
         flex-direction: column;
       `}
+    }
+  `}
+`;
+export const Field = styled.div`
+  ${({ theme }) => css`
+    span {
+      color: ${theme.colors.ternary};
+      font-size: ${theme.font.size.xsm};
+      font-weight: 500;
     }
   `}
 `;
