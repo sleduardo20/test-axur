3a83518f9d61e9ff181ee4c3f1312532cfde04d7
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-09 20:43:36 -0300

    ⚡ feat (app): add progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import { InpectionsProvider } from 'hooks/useInspections';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import NextProgressBar from 'nextjs-progressbar';
+
+import { InpectionsProvider } from 'hooks/useInspections';
 
 import GlobalStyles from 'styles/GlobalStyles';
 import theme from '../styles/theme';
@@ -10,6 +12,12 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <InpectionsProvider>
+        <NextProgressBar
+          color="#FF5824"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+        />
         <Head>
           <title>Axur-Teste Front-End</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
