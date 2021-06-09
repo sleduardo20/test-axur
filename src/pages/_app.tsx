b69c85939ee8df6f1e213521c53d4bac64bf4d71
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-09 09:38:07 -0300

    ⚡ feat (app): add InpectionsProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { InpectionsProvider } from 'hooks/useInspections';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
@@ -8,23 +9,25 @@ import theme from '../styles/theme';
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <title>Axur-Teste Front-End</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-192.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="Projeto para teste de Desenvolvedor Front End"
-        />
-      </Head>
-      <Component {...pageProps} />
-      <GlobalStyles />
+      <InpectionsProvider>
+        <Head>
+          <title>Axur-Teste Front-End</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-192.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="Projeto para teste de Desenvolvedor Front End"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </InpectionsProvider>
     </ThemeProvider>
   );
 }
